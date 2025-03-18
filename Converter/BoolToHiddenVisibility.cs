5a4afb4dcf1366d5c687abf7f097fb8778e99dd9
Christoph Bornhardt <cbornhardt@outlook.com>
2025-03-18 16:48:17 -0300

    bool converters

---
+++ Converter/BoolToHiddenVisibility.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WindowsAppCoreLib.Converter
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BoolToHiddenVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b = (bool)value;
+            if(b)
+                return Visibility.Visible;
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility v = (Visibility)value;
+
+            if (v == Visibility.Visible)
+                return true;
+
+            return false;
+        }
+    }
+}
