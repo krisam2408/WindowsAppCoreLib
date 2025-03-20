bf7b985ac14d31b0f567a1166e5125c96a8baec1
Christoph Bornhardt <cbornhardt@outlook.com>
2025-03-20 15:52:49 -0300

    components start

---
+++ Converter/NullImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WindowsAppCoreLib.Converter;
+
+public sealed class NullImage : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value is null)
+            return DependencyProperty.UnsetValue;
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
---
+++ DataTransfer/TitleBarDragActionArgs.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace WindowsAppCoreLib.DataTransfer;
+
+public sealed class TitleBarDragActionArgs
+{
+    public Window Window { get; set; }
+    public string Name { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public TitleBarDragActionArgs(Window window)
+    {
+        Window = window;
+        X = window.Left;
+        Y = window.Top;
+
+        Name = window
+            .GetType()
+            .Name;
+    }
+}
